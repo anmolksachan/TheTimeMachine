e4c17dfbace7a98623171ec2d2d3cf9a1579c73a
Anmol K Sachan <anmol221999@gmail.com>
2022-10-14 22:56:52 +0530

    Update thetimemachine.py

--- thetimemachine.py
+++ thetimemachine.py
@@ -6,6 +6,66 @@ from colorama import init
 from termcolor import colored
 import os
 import datetime
+import time
+
+def load_animation():
+
+        load_str = "preparing the time machine portal for voyager...."
+        ls_len = len(load_str)
+
+
+        animation = "|/-\\"
+        anicount = 0
+
+        counttime = 0
+
+        i = 0
+
+        while (counttime != 100):
+
+
+                time.sleep(0.075)
+
+
+                load_str_list = list(load_str)
+
+                x = ord(load_str_list[i])
+
+                y = 0
+
+                if x != 32 and x != 46:
+                        if x>90:
+                                y = x-32
+                        else:
+                                y = x + 32
+                        load_str_list[i]= chr(y)
+
+                res =''
+                for j in range(ls_len):
+                        res = res + load_str_list[j]
+
+                sys.stdout.write("\r"+res + animation[anicount])
+                sys.stdout.flush()
+
+
+                load_str = res
+
+
+                anicount = (anicount + 1)% 4
+                i =(i + 1)% ls_len
+                counttime = counttime + 1
+
+        if os.name =="nt":
+                os.system("cls")
+
+        else:
+                os.system("clear")
+
+if __name__ == '__main__':
+        load_animation()
+
+        s ="!"
+        sys.stdout.write("Ready to Travel"+str(s)+"\n")
 
 init()
 print("""
