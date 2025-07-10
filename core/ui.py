2c3cdce10dc63301e5e18f26470600ae51b58dce
Chaudhary_S4h4b <66072013+chaudharyarjun@users.noreply.github.com>
2025-07-10 02:29:22 -0400

    Create ui.py

---
+++ core/ui.py
@@ -0,0 +1,43 @@
+# core/ui.py
+import sys, time, os
+from colorama import init
+from termcolor import colored
+
+def show_loader():
+    load_str = "preparing the time machine portal for voyager...."
+    animation = "|/-\\"
+    counttime = 0
+    i = 0
+    while counttime != 100:
+        time.sleep(0.075)
+        load_str_list = list(load_str)
+        x = ord(load_str_list[i])
+        y = x - 32 if x > 90 else x + 32
+        if x != 32 and x != 46:
+            load_str_list[i] = chr(y)
+        res = ''.join(load_str_list)
+        sys.stdout.write("\r" + res + animation[counttime % 4])
+        sys.stdout.flush()
+        load_str = res
+        counttime += 1
+        i = (i + 1) % len(load_str)
+   # os.system("cls" if os.name == "nt" else "clear")
+    print()
+
+def print_banner():
+    init()
+    print("""
+████████╗██╗░░██╗███████╗  ████████╗██╗███╗░░░███╗███████╗
+╚══██╔══╝██║░░██║██╔════╝  ╚══██╔══╝██║████╗░████║██╔════╝
+░░░██║░░░███████║█████╗░░  ░░░██║░░░██║██╔████╔██║█████╗░░
+░░░██║░░░██╔══██║██╔══╝░░  ░░░██║░░░██║██║╚██╔╝██║██╔══╝░░
+░░░██║░░░██║░░██║███████╗  ░░░██║░░░██║██║░╚═╝░██║███████╗
+░░░╚═╝░░░╚═╝░░╚═╝╚══════╝  ░░░╚═╝░░░╚═╝╚═╝░░░░░╚═╝╚══════╝
+
+███╗░░░███╗░█████╗░░█████╗░██╗░░██╗██╗███╗░░██╗███████╗
+████╗░████║██╔══██╗██╔══██╗██║░░██║██║████╗░██║██╔════╝
+██╔████╔██║███████║██║░░╚═╝███████║██║██╔██╗██║█████╗░░
+██║╚██╔╝██║██╔══██║██║░░██╗██╔══██║██║██║╚████║██╔══╝░░
+██║░╚═╝░██║██║░░██║╚█████╔╝██║░░██║██║██║░╚███║███████╗
+╚═╝░░░░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝╚═╝╚═╝░░╚══╝╚══════╝""")
+    print(colored('  Coded with Love by Anmol K Sachan (@Fr13ND0x7f) & Arjun Chaudhary (Chaudhary_S4h4b)\n','green')) 
